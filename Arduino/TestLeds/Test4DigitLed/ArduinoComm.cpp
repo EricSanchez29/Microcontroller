4f927589b305352b42866ff3655c8e5e46e09274
Eric Sanchez <ericsanchez29@gmail.com>
2022-07-14 19:31:59 -0500

    Finally working

---
+++ Arduino/TestLeds/Test4DigitLed/ArduinoComm.cpp
@@ -0,0 +1,65 @@
+#include "ArduinoComm.h"  
+
+// Need default constructor 
+ArduinoComm::ArduinoComm(){}
+
+// Pins 0-12 (nano doesn't have pin 13)
+// Ceate another constructor for more custom mapping?
+ArduinoComm::ArduinoComm(int firstPin, int lastPin)
+{
+    if (lastPin - firstPin != 7)
+    {
+        return;
+    }
+
+    int j = 0;
+
+    for (int i = firstPin; i <= lastPin; i++)
+    {
+        _pinMap[j] = i;
+        j++;
+    }
+
+    return;
+}
+
+void ArduinoComm::Write(byte data)
+{    
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        uint8_t pin = _pinMap[i];
+        pinMode(pin, OUTPUT);
+        digitalWrite((int)pin, _getPinData(data, i));
+    }
+    
+   return;
+}
+
+bool ArduinoComm::_getPinData(byte data, uint8_t bit)
+{
+    data = data>>(7-bit);
+
+    data = data ^ B11111110;
+
+    if (data & B00000001)
+    {
+        return  true;
+    }
+
+    return false;
+}
+
+byte ArduinoComm::Read()
+{
+    byte result;
+
+    for (int i = 0; i < 8; i++)
+    {
+        uint8_t pin = _pinMap[i];
+        pinMode(pin, INPUT);
+        result = result && digitalRead(pin);
+        result<<1;
+    }
+    
+   return result;
+}
