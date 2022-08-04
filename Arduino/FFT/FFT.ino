b06379abfcec9c6e0982c42aedaf2c05d6f1acc2
Eric Sanchez <ericsanchez29@gmail.com>
2022-08-03 20:18:23 -0500

    When FFT code is done will update this sketch

--- Arduino/FFT/FFT.ino
+++ Arduino/FFT/FFT.ino
@@ -4,6 +4,18 @@
 #include "ArduinoCommNybl.h"
 #include "FourDigitLed.h" 
 
+/*
+    Right now I am just seeing how much space this takes up when its compiled for arduino
+    
+    Sketch uses 3424 bytes (11%) of program storage space. Maximum is 30720 bytes.
+    Global variables use 700 bytes (34%) of dynamic memory, leaving 1348 bytes for local variables. Maximum is 2048 bytes.
+
+
+    Will need to add code to communicate with microphone
+    // So far I seem to have enough storage space, dynamic memory might be used up?
+*/
+
+
 void setup() 
 {
     uint16_t* wave_time; 
